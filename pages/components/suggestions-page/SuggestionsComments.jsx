0c926d31a50894068c90a675f3f19f5e52de75e3
morfeli <fmoreira95@hotmail.com>
2022-04-06 14:00:59 -0400

    new: rendering specific comments in the single feedback page

--- pages/components/suggestions-page/SuggestionsComments.jsx
+++ pages/components/suggestions-page/SuggestionsComments.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
 const SuggestionsComments = ({ item }) => {
-  return <h1>{console.log(item)}</h1>;
+  const comments = item.map((item) => item.comments);
+
+  const content = comments[0].map((item) => <li>{item.content}</li>);
+  console.log(content);
+  const totalLength = item.map((item, i) => {
+    let comments = item.comments;
+    let repliesArr = comments ? comments.map((item) => item.replies) : null;
+
+    let totalCommentsLength;
+    if (comments && repliesArr) {
+      totalCommentsLength = comments.length + repliesArr.length;
+    }
+
+    return totalCommentsLength;
+  });
+
+  return (
+    <section>
+      <div className="flex p-8">
+        <span>{totalLength}</span>
+        <h1 className="pl-2">Comments</h1>
+      </div>
+      <ul>
+        {comments ? comments[0].map((item) => <li>{item.content}</li>) : null}
+      </ul>
+    </section>
+  );
 };
 
 export default SuggestionsComments;
--- pages/suggestions/[id].jsx
+++ pages/suggestions/[id].jsx
@@ -1,5 +1,4 @@
 import { buildFeedbackPath, extractFeedback } from "../helper/HelperFunctions";
-
 import { IconArrowSvg } from "../components/dashboard-ui/UI/IconArrowSvg";
 import { CommentsSvg } from "../components/dashboard-ui/UI/CommentsSvg";
 import SuggestionsComments from "../components/suggestions-page/SuggestionsComments";
@@ -7,7 +6,7 @@ import SuggestionsComments from "../components/suggestions-page/SuggestionsComme
 const SuggestionFeedbackDetailPage = ({ item }) => {
   return (
     <>
-      <ul>
+      <ul className="mt-8">
         {item.map((item, i) => {
           let comments = item.comments;
           let repliesArr = comments
@@ -19,12 +18,14 @@ const SuggestionFeedbackDetailPage = ({ item }) => {
             totalCommentsLength = comments.length + repliesArr.length;
           }
           return (
-            <li key={item.id}>
-              <h1>{item.title}</h1>
-              <p>{item.description}</p>
-              <button>{item.category}</button>
-              <div>
-                <button>
+            <li key={item.id} className="p-4 mx-4 bg-white rounded-2xl">
+              <h1 className="text-third-blue font-jost-bold">{item.title}</h1>
+              <p className="py-2 text-first-blue">{item.description}</p>
+              <button className="p-2 capitalize text-second-blue rounded-xl bg-light-gray font-jost-semibold">
+                {item.category}
+              </button>
+              <div className="flex justify-between pt-4">
+                <button className="flex items-center p-2 capitalize text-second-blue rounded-xl bg-light-gray font-jost-bold">
                   <IconArrowSvg />
                   {item.upvotes}
                 </button>
