87af75cb3929530b8f75808206119400142b32b8
morfeli <fmoreira95@hotmail.com>
2022-04-29 11:59:12 -0400

    new: now styling app towards desktop view

--- pages/components/suggestions-page/UserReplies.jsx
+++ pages/components/suggestions-page/UserReplies.jsx
@@ -14,17 +14,16 @@ const UserReplies = ({
 }) => {
   return (
     <>
-      <div className="absolute left-90px">
-        {image ? (
-          <Image src={image} width={50} height={50} className="rounded-3xl " />
-        ) : null}
-      </div>
-      <div className="flex items-center justify-between px-6">
+      <div className="flex items-center justify-between pl-8">
         <div className="flex items-center">
-          <div>
+          {image ? (
+            <Image src={image} width={50} height={50} className="rounded-3xl" />
+          ) : null}
+
+          <div className="flex flex-col pl-4">
             <h1 className="font-jost-semibold text-first-blue">{name}</h1>
             {firstName && lastName ? (
-              <div className="flex items-center">
+              <div className="flex">
                 <h1 className="font-jost-semibold text-first-blue">
                   {firstName}
                 </h1>
@@ -44,7 +43,7 @@ const UserReplies = ({
           Reply
         </h1>
       </div>
-      <div className="flex px-4 py-2">
+      <div className="flex py-2">
         {content ? (
           <p className="text-sm md:text-base">
             <span className="pr-2 text-button-pink font-jost-bold md:text-base">
