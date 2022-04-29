f8d5d4f0f98b8dd8410d260f9fca1d1c4281f6e0
morfeli <fmoreira95@hotmail.com>
2022-04-29 13:46:42 -0400

    new: cleaning up npm run build errors

--- pages/api/signup.jsx
+++ pages/api/signup.jsx
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import { hashedPassword, connectToDatabase } from "../helper/HelperFunctions";
 
 const isEmpty = (value) => value.trim() === "";
