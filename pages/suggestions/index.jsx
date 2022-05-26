928483e417678495ae5ebf515d001d0abec9453a
morfeli <fmoreira95@hotmail.com>
2022-05-26 18:57:33 -0400

    trying a different approach with useSWR

--- pages/suggestions/index.jsx
+++ pages/suggestions/index.jsx
@@ -1,25 +1,56 @@
 import { useState, useEffect } from "react";
 import { getSession } from "next-auth/react";
-
-import path from "path";
-import fs from "fs";
+import useSWR from "swr";
 
 // components
+
 import Dashboard from "../../components/dashboard-ui/Dashboard";
 import SortingHeader from "../../components/dashboard-ui/SortingHeader";
 import Suggestions from "../../components/suggestions-page/Suggestions";
 
-import { filteredData } from "../../helper/HelperFunctions";
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
-const SuggestionsPage = ({ session, feedbackData }) => {
-  const { suggestions, progress, planned, live } = feedbackData;
+const SuggestionsPage = ({ session }) => {
+  const [loadedData, setLoadedData] = useState(null);
+  const [filter, setFilter] = useState(null);
   const [sort, setSort] = useState("Most Upvotes");
   const [category, setCategory] = useState("all");
-  const [filter, setFilter] = useState(suggestions);
-  const [suggestionLength, setSuggestionLength] = useState(filter.length);
+  // const [suggestionLength, setSuggestionLength] = useState(filter.length);
   const [innerWidth, setInnerWidth] = useState(0);
-
   const isMobile = innerWidth <= 767;
+
+  const { data, error } = useSWR("/api/staticdata", fetcher);
+
+  console.log(data);
+
+  useEffect(() => {
+    if (data) {
+      const inProgressStatusData = data.productRequests.filter(
+        (item) => item.status == "in-progress"
+      );
+
+      const liveStatusData = data.productRequests.filter(
+        (item) => item.status == "live"
+      );
+
+      const plannedStatusData = data.productRequests.filter(
+        (item) => item.status == "planned"
+      );
+
+      let filterData = data.productRequests.filter(
+        (item) => item.status == "suggestion"
+      );
+
+      let feedbackData = {
+        suggestions: filterData,
+        progress: inProgressStatusData,
+        planned: plannedStatusData,
+        live: liveStatusData,
+      };
+
+      setLoadedData(feedbackData);
+    }
+  }, []);
 
   const changeWidth = () => setInnerWidth(window.innerWidth);
 
@@ -33,19 +64,19 @@ const SuggestionsPage = ({ session, feedbackData }) => {
     };
   }, [isMobile]);
 
-  let roadmapData = {
-    progress,
-    planned,
-    live,
-  };
+  // let roadmapData = {
+  //   progress,
+  //   planned,
+  //   live,
+  // };
 
   const filterDataByCategory = (category) => {
     if (category === "all") {
-      let filteredFeedbacks = suggestions;
+      let filteredFeedbacks = loadedData.suggestions;
       setFilter(filteredFeedbacks);
       setSuggestionLength(filteredFeedbacks.length);
     } else {
-      let filteredFeedbacks = suggestions.filter(
+      let filteredFeedbacks = loadedData.suggestions.filter(
         (item) => item.category === category
       );
       setFilter(filteredFeedbacks);
@@ -58,7 +89,7 @@ const SuggestionsPage = ({ session, feedbackData }) => {
     if (filter) {
       arr = filter;
     } else {
-      arr = suggestions;
+      arr = loadedData.suggestions;
     }
 
     switch (sort) {
@@ -158,7 +189,8 @@ const SuggestionsPage = ({ session, feedbackData }) => {
   if (session) {
     return (
       <main className="xl:flex xl:justify-evenly">
-        <div>
+        <h1>hello world</h1>
+        {/* <div>
           <Dashboard
             isMobile={isMobile}
             innerWidth={innerWidth}
@@ -182,21 +214,13 @@ const SuggestionsPage = ({ session, feedbackData }) => {
             isMobile={isMobile}
             innerWidth={innerWidth}
           />
-        </div>
+        </div> */}
       </main>
     );
   }
 };
 
 export default SuggestionsPage;
-
-async function getData() {
-  const filePath = path.join(process.cwd(), "public", "data", "data.json");
-  const jsonData = await fs.readFileSync(filePath);
-  const data = JSON.parse(jsonData);
-
-  return data;
-}
 
 export const getServerSideProps = async (context) => {
   const session = await getSession({ req: context.req });
@@ -209,31 +233,8 @@ export const getServerSideProps = async (context) => {
       },
     };
   } else {
-    const data = await getData();
-
-    const inProgressStatusData = data.productRequests.filter(
-      (item) => item.status == "in-progress"
-    );
-
-    const liveStatusData = data.productRequests.filter(
-      (item) => item.status == "live"
-    );
-
-    const plannedStatusData = data.productRequests.filter(
-      (item) => item.status == "planned"
-    );
-
-    let filterData = filteredData(data, "suggestion");
-
-    let feedbackData = {
-      suggestions: filterData,
-      progress: inProgressStatusData,
-      planned: plannedStatusData,
-      live: liveStatusData,
-    };
-
     return {
-      props: { session, feedbackData },
+      props: { session },
     };
   }
 };
