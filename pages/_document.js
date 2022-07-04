6928dfed218b71b3c26b9f5f3f50237ef7833cd3
morfeli <fmoreira95@hotmail.com>
2022-07-04 16:00:37 -0400

    few changes

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,8 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="icon" href="/favicon.ico" />
+
           <link
             href="https://fonts.googleapis.com/css2?family=Jost:wght@400;600;700&display=swap"
             rel="stylesheet"
